d2e1eb169504766eb572bd935cad56460f193dc2
ButlerZhang <zwq1011@163.com>
2018-08-24 14:34:31 +0800

    Deleted Example27 project.

--- LingLite/Example36/main.py
+++ LingLite/Example36/main.py
@@ -2,7 +2,7 @@ import sys
 print(sys.version)
 print('\n')
 
-sourceFileName = r"Naughty Brother.txt"
+sourceFileName = r"DataSource//Naughty Brother.txt"
 resultFileName1 = r"ResultFiles//result1.txt"
 resultFileName2 = r"ResultFiles//result2.txt"
 
