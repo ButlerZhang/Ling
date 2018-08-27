b29af94090460921603f0a29af7091d0da041e77
ButlerZhang <zwq1011@163.com>
2018-08-27 17:21:15 +0800

    Add Example7, Distributed process.

--- LingLite/Example36/main.py
+++ LingLite/Example36/main.py
@@ -6,18 +6,6 @@ g_sourceFileName = r"DataSource//Naughty Brother.txt"
 g_resultFileName1 = r"ResultFiles//result1.txt"
 g_resultFileName2 = r"ResultFiles//result2.txt"
 
-import os
-import Example6_Coroutine as E6
-from gevent.pool import Pool
-
-from gevent import monkey
-monkey.patch_all()
-import gevent
-
-
-
-if __name__ == "__main__":
-    pool = Pool(2)
-    urls = [r"https://github.com/", r"https://www.python.org/", r"http://www.cnblogs.com/"]
-    results = pool.map(E6.run_task, urls)
-    print(results)
+#import Example7_taskManager as E7taskManager
+import Example7_taskWorker as work
+work.testWork()
